f86ed76173a6a5f45f3911715a4dac422285f71c
tracer-ics2017 <tracer@njuics.org>
2020-03-21 16:08:32 +0800

    >  compile 161820215 wangyunlong Linux 10 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  16:08:32 up  1:58,  2 users,  load average: 0.00, 0.00, 0.00 4fb3f794420c659ec334dc2ccc415d7340af5a77

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -6,7 +6,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INSTR_TO_PRINT 21//将10变为21
+#define MAX_INSTR_TO_PRINT 211//将10变为211
 
 int nemu_state = NEMU_STOP;
 
