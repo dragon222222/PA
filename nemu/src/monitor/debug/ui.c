4f3d3b2af90243024c5a3c77aac50fd4c67a0b3c
tracer-ics2017 <tracer@njuics.org>
2020-03-22 19:21:53 +0800

    >  compile 161820215 wangyunlong Linux 10 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  19:21:53 up  3:56,  2 users,  load average: 0.04, 0.04, 0.00 9f1b32d628aa50ea925f05cde1564261738f8aa7

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -140,8 +140,9 @@ static int cmd_x(char *args){
   for(i=0;i<m;i++){
   	printf("%#x    ",addr);
 	n=vaddr_read(addr,4);
-	printf("%#x    ",n);
+	printf("%#x     ",n);
 	show_bytes((byte_pointer)&n,sizeof(int));
+	addr+=4;//每次加4
   }
   return 0;
 }
