06d5a977dfceb7225a713d8a9432ec3cb5ebb4e0
tracer-ics2017 <tracer@njuics.org>
2020-04-11 20:21:22 +0800

    >  compile 161820215 wangyunlong Linux 10 4.19.0-8-amd64 #1 SMP Debian 4.19.98-1 (2020-01-26) x86_64 GNU/Linux  20:21:22 up  5:51,  2 users,  load average: 0.00, 0.00, 0.00 9fefad19b3d631d0fb72db955bdc5fee4c033817

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -151,7 +151,7 @@ static bool make_token(char *e) {
                                  Auxiliary(i,substr_len,substr_start);
                                  nr_token++;
                          }break;
-                default:assert(0);//出错
+                //default:assert(0);//出错
         }
         break;
       }
@@ -170,17 +170,17 @@ bool check_parentheses(int p,int q){
   int left=0;
   int right=0;
   if(!(tokens[p].type=='('&&tokens[q].type==')'))
-	  return false;
+          return false;
   for(p=p+1;p<q;p++){
-  	if(tokens[p].type=='(')
-		left++;
-	if(tokens[p].type==')')
-		right++;
-	if(right>left)
-		return false;//任何时刻右括号都不会多于左括号
+        if(tokens[p].type=='(')
+                left++;
+        if(tokens[p].type==')')
+                right++;
+        if(right>left)
+                return false;//任何时刻右括号都不会多于左括号
   }
   if(right!=left)
-	  return false;
+          return false;
   return true;
 }
 
@@ -190,22 +190,22 @@ uint32_t find_dominated_op(int p,int q){
   int sum=0;//这个是来保证中心运算符在括号外面
   for(;p<q;p++){
     if(tokens[p].type=='(')
-	    sum++;
+            sum++;
     if(tokens[p].type==')')
-	    sum--;
+            sum--;
     if(sum==0){
       if(tokens[p].type==TK_EQ||tokens[p].type==TK_UEQ||tokens[p].type=='&'||tokens[p].type=='|')
-	      return p;//这些运算符肯定是
+              return p;//这些运算符肯定是
       if(tokens[p].type==TK_IAC)
-	      continue;//指针解引用直接“放弃”
+              continue;//指针解引用直接“放弃”
       if(tokens[p].type=='+')
-      	      x=p;//虽然是最低的，但是要根据结合性	
+              x=p;//虽然是最低的，但是要根据结合性
       if(tokens[p].type=='-')
-	      if(tokens[p-1].type!='-'||tokens[p-1].type!='+'||tokens[p-1].type!='*'||tokens[p-1].type!='/')
-		      x=p;//后面要求加入负号，所以判断一下
+              if(tokens[p-1].type!='-'||tokens[p-1].type!='+'||tokens[p-1].type!='*'||tokens[p-1].type!='/')
+                      x=p;//后面要求加入负号，所以判断一下
       if(tokens[p].type=='*'||tokens[p].type=='/')
-	      if(tokens[x].type!='+'||tokens[x].type!='-')
-		      x=p;//如果没有比乘除级别低的，就选乘除,有点矮子头上选将军的感觉
+              if(tokens[x].type!='+'||tokens[x].type!='-')
+                      x=p;//如果没有比乘除级别低的，就选乘除,有点矮子头上选将军的感觉
     }
   }
   return x;//此时x就是结合了运算级别以及结合性的结果
@@ -218,16 +218,16 @@ uint32_t eval(int p,int q){
   else if(p == q) {//单个表达式，只能为数字或者寄存器的情况
     int num= 0;
     if(tokens[p].type == TK_ST)//十六进制转化成十六进制数
-    	  sscanf(tokens[p].str, "%x",&num);
+          sscanf(tokens[p].str, "%x",&num);
     if(tokens[p].type == TK_TEN)//十进制数转化成十进制数
-    	  sscanf(tokens[p].str, "%d",&num);
+          sscanf(tokens[p].str, "%d",&num);
     if(tokens[p].type == TK_REG) {//是寄存器
-      for(int i = 0;i < 4;i ++) 
+      for(int i = 0;i < 4;i ++)
         tokens[p].str[i] = tokens[p].str[i + 1];//把$号去掉
       if(strcmp(tokens[p].str,"eip") == 0) //如果是eip寄存器
         num= cpu.eip;
-      else 
-          for(int i = 0;i < 8;i ++) 
+      else
+          for(int i = 0;i < 8;i ++)
             if(strcmp(tokens[p].str,regsl[i]) == 0) {//规定只有32位寄存器
               num=cpu.gpr[i]._32;
               break;
@@ -239,25 +239,25 @@ uint32_t eval(int p,int q){
     return eval(p + 1, q - 1);
   }
   else{
-    int x,val1,val2;
+    int x,m,n;
     x= find_dominated_op(p,q);
     if(x==p&&tokens[p].type==TK_IAC)//指针解引用情况
-	    return vaddr_read(eval(p+1,q),4);//此时的地址
+            return vaddr_read(eval(p+1,q),4);//此时的地址
     if(x==p&&tokens[p].type=='-')//负数表达式取负
-	    return -eval(x+1,q);
+            return -eval(x+1,q);
     if(x==p&&tokens[p].type=='!')//取非
-	    return !eval(x+1,q);
-    val1 = eval(p, x-1);
-    val2 = eval(x+1, q);
+            return !eval(x+1,q);
+    m= eval(p, x-1);
+    n= eval(x+1, q);
     switch (tokens[x].type) {
-      case '+': return val1 + val2;
-      case '-': return val1 - val2;
-      case '*': return val1 * val2;
-      case '/': return val1 / val2;
-      case TK_EQ: return val1 == val2;
-      case TK_UEQ: return val1 != val2;
-      case '&': return val1 & val2;
-      case '|': return val1 | val2;
+      case '+': return m + n;
+      case '-': return m- n;
+      case '*': return m * n;
+      case '/': return m/ n;
+      case TK_EQ: return m == n;
+      case TK_UEQ: return m!= n;
+      case '&': return m& n;
+      case '|': return m|n;
       default: assert(0);
     }
   }
@@ -272,8 +272,8 @@ uint32_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   for(int i=0;i<nr_token;i++)
-	  if(tokens[i].type=='*'&&(i==0||tokens[i-1].type=='+'||tokens[i-1].type=='-'||tokens[i-1].type=='*'||tokens[i-1].type=='/'))
-		  tokens[i].type=TK_IAC;
+          if(tokens[i].type=='*'&&(i==0||tokens[i-1].type=='+'||tokens[i-1].type=='-'||tokens[i-1].type=='*'||tokens[i-1].type=='/'))
+                  tokens[i].type=TK_IAC;
   int p=0;
   int q=nr_token-1;
   return eval(p,q);
