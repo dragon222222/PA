f9f43929e1ede2ef1b8f53d9dd15ddefa24de9d9
tracer-ics2017 <tracer@njuics.org>
2020-06-28 21:19:00 +0800

    >  run 161820215 wangyunlong Linux debian 4.19.0-9-amd64 #1 SMP Debian 4.19.118-2 (2020-04-29) x86_64 GNU/Linux  21:19:00 up  1:11,  1 user,  load average: 0.00, 0.00, 0.00 7c015376b4f3e9a91c75a1a231dc54d4709d55fb

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,12 +1,12 @@
 #include "common.h"
 _RegSet* do_syscall(_RegSet *r);
-
+_RegSet* schedule(_RegSet *prev);
 static _RegSet* do_event(_Event e, _RegSet* r) {
   switch (e.event) {
     case _EVENT_SYSCALL: 
 	    do_syscall(r);
 	    break;
-    case _EVENT_TRAP: Log("_EVENT_TRAP");return NULL;
+    case _EVENT_TRAP: return schedule(r);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
